b3ffff2562a06d393d20db1e89668d58727d17f7
bilkish-akther <bilkishakther921@gmaile.com>
2023-05-21 05:40:00 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,132 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const  port = process.env.PORT || 5000;
+
+
+// middleware 
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.efwmi0g.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+
+    // const serviceCollection = client.db('toyUser').collection('toyService');
+    const toysCollection = client.db('dollUser').collection('allDoll');
+
+    app.get('/toyService', async(req ,res ) => {
+
+        const cursor = serviceCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    
+       })
+
+
+        //add toy in dataBase
+   app.post("/post-toy", async (req, res) => {
+    const body = req.body;
+    console.log(body);
+    const result = await toysCollection.insertOne(body);
+    if (result?.insertedId) {
+      return res.status(200).send(result);
+    } else {
+      return res.status(404).send({
+        message: "can not insert try again later",
+        status: false,
+      });
+    }
+  });
+
+
+
+
+   //update toy in dataBase
+   app.put("/updateToy/:id", async (req, res) => {
+    const id = req.params.id;
+    const body = req.body;
+    console.log(body);
+    const filter = { _id: new ObjectId(id) };
+    const updateDoc = {
+      $set: {
+        price: body.price,
+        quantity: body.quantity,
+        description: body.description,
+      },
+    };
+    const result = await toysCollection.updateOne(filter, updateDoc);
+    res.send(result);
+  });
+
+
+
+  
+
+   //Delete toy in dataBase
+
+   app.delete("/deleteToy/:id", async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) }
+    const result = await toysCollection.deleteOne(query);
+    res.send(result);
+  });
+
+
+
+
+  
+  //get Toy List from DB
+
+  app.get('/toysList', async(req ,res ) => {
+    const cursor = toysCollection.find().limit(20);
+    const result = await cursor.toArray();
+    res.send(result);
+   })
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+
+
+
+run().catch(console.dir);
+
+
+
+
+
+
+
+
+
+app.get('/', (req,res) => {
+
+    res.send('Toy Market Place is running ')
+    })
+    
+    app.listen(port , () =>{
+        console.log(`toy Market Place Server is Running on port ${port}`);
+    })
+    
